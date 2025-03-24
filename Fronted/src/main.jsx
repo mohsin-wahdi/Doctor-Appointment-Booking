e9451aa595cfb1f91eae0b30ed97eb644c55d4d9
mohsin-wahdi <amman4781@gmail.com>
2025-03-24 23:59:22 +0530

    changes

--- Fronted/src/main.jsx
+++ Fronted/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
+import AppContextProvider from "./context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
   </BrowserRouter>
 );
