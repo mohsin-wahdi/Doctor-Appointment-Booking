3d96d76ab755b1a1406b46057db460456b77da46
mohsin-wahdi <amman4781@gmail.com>
2025-03-26 23:45:11 +0530

    changes

--- Fronted/src/pages/Home.jsx
+++ Fronted/src/pages/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import Header from "../components/Header";
+import SpecialityMenu from "../components/SpecialityMenu";
+import TopDoctor from "../components/TopDoctor";
+import Banner from "../components/Banner";
+import Footer from "../components/Footer";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+      <SpecialityMenu />
+      <TopDoctor />
+      <Banner />
+    </div>
+  );
 };
 
 export default Home;
