83a5eaedf2f487669d4842e9640240f3d2fdb423
mohsin-wahdi <amman4781@gmail.com>
2025-03-28 23:14:26 +0530

    changes

--- Fronted/src/pages/MyAppointments.jsx
+++ Fronted/src/pages/MyAppointments.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
-
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 const MyAppointments = () => {
+  const { doctors } = useContext(AppContext);
   return (
     <div>
-      MyAppointments
+      <p className="pb-3 mt-12 font-medium text-zinc-700 border-b">
+        My Appointments
+      </p>
+      <div>
+        {doctors.slice(0, 2).map((item, index) => (
+          <div
+            className="grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b border-zinc-200"
+            key={index}
+          >
+            <div>
+              <img className="w-32 bg-indigo-50" src={item.image} alt="" />
+            </div>
+            <div className="flex-1 text-sm text-zinc-600">
+              <p>{item.name}</p>
+              <p>{item.speciality}</p>
+              <p>Address:</p>
+              <p>{item.address.line1}</p>
+              <p>{item.address.line2}</p>
+              <p>
+                <span>Date & Time:</span> 26 Apr 2025 | 01:30 PM
+              </p>
+            </div>
+            <div></div>
+            <div>
+              <button>Pay Online</button>
+              <button>Cancel Appointment</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MyAppointments
+export default MyAppointments;
