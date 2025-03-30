c4f1e2b3a4e4a62cb9f375ab765ff6203891042c
mohsin-wahdi <amman4781@gmail.com>
2025-03-30 22:38:42 +0530

    changes

--- Fronted/src/context/AppContext.jsx
+++ Fronted/src/context/AppContext.jsx
@@ -2,10 +2,12 @@ import { createContext } from "react";
 import { doctors } from "../assets/assets";
 
 export const AppContext = createContext();
+const CurrencySymbol = "₹";
 
 const AppContextProvider = (props) => {
   const value = {
     doctors,
+    CurrencySymbol,
   };
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
